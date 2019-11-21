42e8c8ca337b2985196550d2cd1b8653fd6e92e9
alexkalinchyk <ak@winmixsoft.com>
2019-11-21 20:15:20 +0100

    Swipe component + assets

---
+++ src/views/Swipe.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="swipe">
+    <h1>This is a swipe page</h1>
+    <SwipeCards />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SwipeCards from '@/components/SwipeCards.vue'
+
+export default {
+  name: 'swipe',
+  components: {
+    SwipeCards
+  }
+}
+</script>
